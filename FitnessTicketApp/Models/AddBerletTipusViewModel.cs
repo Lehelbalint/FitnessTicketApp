9cd3cbb8a684764b5afcd025ef120237a202e1cf
Horvath99 <79415435+Horvath99@users.noreply.github.com>
2023-05-11 19:59:25 +0300

    BerletTipusok
    Create,Add,Edit working

---
+++ FitnessTicketApp/Models/AddBerletTipusViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FitnessTicketApp.Models
+{
+    public class AddBerletTipusViewModel
+    {
+        public string Megnevezes { get; set; }
+
+        public int Ar { get; set; }
+
+        public int HanyNapigErvenyes { get; set; }
+
+        public int HanyBelepesreErvenyes { get; set; }
+
+        public bool Torolve { get; set; }
+
+        public int Terem_Id { get; set; }
+
+        public int Hanyoratol { get; set; }
+
+        public int Hanyoraig { get; set; }
+
+        public int NapontaHanyszorHasznalhato { get; set; }
+    }
+}
