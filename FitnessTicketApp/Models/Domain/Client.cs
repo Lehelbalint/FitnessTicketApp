fadfedc07c2a5daecde516305d1e1a19e1f46f62
Lehelbalint <balint.lehel@student.ms.sapientia.ro>
2023-05-11 19:53:18 +0300

    Clients/Create/Delete/Update/Search/Get

---
+++ FitnessTicketApp/Models/Domain/Client.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessTicketApp.Models.Domain
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public Boolean Is_deleted { get; set; }
+        public byte[]? Photo  { get; set; }
+        public DateTime inserted_date { get; set; }
+        public string CNP { get; set; }
+        public string Adress { get; set; }
+        public string Code { get; set; }
+        public string Comment { get; set; }
+         
+    }
+}
